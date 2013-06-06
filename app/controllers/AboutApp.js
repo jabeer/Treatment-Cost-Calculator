e9d6fdb6916f200e03cadaa2012dd0697adc13e6
Jabeer Ali <ajabeerali@virtusa.com>
2013-06-06 17:43:17 +0530

    Modal WIndow Working in Android and Ipad. Not wokring on Iphone.

--- app/controllers/AboutApp.js
+++ app/controllers/AboutApp.js
@@ -1,10 +1,21 @@
+var calc = require('com.andorid.sample');
+
+var nativeView=calc.createExample({message: "hello world"});
+
+$.AboutApp.add(nativeView);
+
+
+
+
 
 
 function closeModelWindow(event){
 	$.AboutApp.close();
 }
+
+
 var closeButton=Ti.UI.createButton({
-	title:"close"
+	systemButton:Ti.UI.iPhone.SystemButton.DONE
 });
 
 
@@ -21,11 +32,21 @@ if(Ti.Platform.osname=="iphone")
 {
 	$.AboutApp.size=Ti.UI.SIZE;
 	$.AboutApp.backgroundColor="FFFFFF";
-}else
-{
+}
 
-alert($.AboutApp.getHeight());
-
-// alert($.AboutApp.width);
+/*
+ * function showAbout() {
+	var aboutApp = Alloy.createController('AboutApp').getView();
+	
+	aboutApp.open({
+		modal : true,
+		modalTransitionStyle : Ti.UI.iPhone.MODAL_TRANSITION_STYLE_FLIP_HORIZONTAL,
+		modalStyle : Ti.UI.iPhone.MODAL_PRESENTATION_FORMSHEET,
+	});
 	
 }
+
+* <Button id="showAbout" onClick="showAbout">About</Button>
+* */
+
+ 
