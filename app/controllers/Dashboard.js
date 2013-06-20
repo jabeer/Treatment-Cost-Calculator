d92996c31bad87991b58be5d8c84f81229d14852
virtusa <virtusa@isai.virtusa.com>
2013-06-20 13:09:43 +0530

    changes for ios and android tab bar navigation

--- app/controllers/Dashboard.js
+++ app/controllers/Dashboard.js
@@ -22,7 +22,22 @@ function showMap()
 function showUrgentCare()
 {
 	
-	//var win3 = Alloy.createController('UrgentCareList').getView();
-   // Alloy.Globals.navBar.open(win3, {animated : true});
-	Alloy.Globals.mainWin.add(Alloy.createController('UrgentCareList').getView());
+	// //var win3 = Alloy.createController('UrgentCareList').getView();
+   // // Alloy.Globals.navBar.open(win3, {animated : true});
+	// Alloy.Globals.mainWin.add(Alloy.createController('UrgentCareList').getView());
+
+Alloy.Globals.searchTap=false;
+Alloy.Globals.favTap=false;
+
+//Alloy.Globals.tabHome.close(Alloy.Globals.mainWin);
+Alloy.Globals.UrgentCareList=Alloy.createController('UrgentCareList').getView();
+if(OS_ANDROID){
+	//Alloy.Globals.tabHome.close();
+	Alloy.Globals.tabHome.open(Alloy.Globals.UrgentCareList);
+
+}
+if(OS_IOS)	
+Alloy.Globals.tabHome.open(Alloy.Globals.UrgentCareList,{animated:true});
+
+
 }
--- app/controllers/tabBar.js
+++ app/controllers/tabBar.js
@@ -1,3 +1,22 @@
-Alloy.Globals.mainWin = $.winHome;
-Alloy.Globals.winSettings = $.winSettings;
-Alloy.Globals.navBar = $.nav;
+// Alloy.Globals.mainWin = $.winHome;
+// 
+// Alloy.Globals.winSearch = $.winSearch;
+// Alloy.Globals.winFav = $.winFav;
+// Alloy.Globals.winSettings = $.winSettings;
+Alloy.Globals.tabGrp=$.tabGrp
+Alloy.Globals.tabHome=$.tabHome;
+Alloy.Globals.tabSearch=$.tabSearch;
+Alloy.Globals.tabFav=$.tabFav;
+Alloy.Globals.tabSettings=$.tabSettings;
+    	
+
+// if((Titanium.UI.currentTab==$.tabHome)||(Titanium.UI.currentTab==$.tabFav))
+// {
+	// alert("hi");
+// Alloy.Globals.searchTap=false;	
+// }
+// if((Titanium.UI.currentTab==$.tabSearch)||(Titanium.UI.currentTab==$.tabHome))
+// {
+// Alloy.Globals.favTap=false;	
+// }
+
