36051621922035b33f2d85c67044b9821690f819
Prachi <virtusa@prachi.virtusa.com>
2013-06-21 14:44:44 +0530

    model

---
+++ app/models/ProfileModel.js
@@ -0,0 +1,24 @@
+exports.definition = {
+	config : {
+		
+	/*	"columns": {
+            "FirstName": "String",
+            "LastName": "String",
+            "Gender":"String",
+            "Age": "String",
+		    "Email": "String",
+		    "Address": "String",
+		    "SchoolAddress": "String",
+		  
+        },*/
+        
+		"adapter" : {
+			"type" : "properties",			
+			"collection_name" : "ProfileModel"
+		}
+	},
+
+
+	
+}
+
